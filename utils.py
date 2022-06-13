4e9ac7a88a82dbeb54bad6716ee936ca57d414f8
ILYESBouhari <bouharidev.io@gmail.com>
2022-06-14 00:08:13 +0100

    update

--- utils.py
+++ utils.py
@@ -1,4 +1,2 @@
-def load_glove_embeddings():
-
-    !wget http://nlp.stanford.edu/data/glove.6B.zip
-    !unzip -q glove.6B.zip
+!wget http://nlp.stanford.edu/data/glove.6B.zip
+!unzip -q glove.6B.zip
