3e24f6385467386fa34a784dbe5fbc568b65cd00
ILYESBouhari <bouharidev.io@gmail.com>
2022-06-14 23:31:20 +0100

    git cache cleared

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from keras.layers import Embedding
 
 def load_glove_embeddings(path, embedding_dim):
 
@@ -17,6 +18,7 @@ def load_glove_embeddings(path, embedding_dim):
     f.close()
   
     print('Loaded %s word vectors.' % len(embeddings_index))
+    
     return embeddings_index
 
 
@@ -29,4 +31,18 @@ def create_embeddings_matrix(embeddings_index, vocabulary, embedding_dim=100):
             embeddings_matrix[i] = embedding_vector
 
     print('Matrix shape: {}'.format(embeddings_matrix.shape))
+
     return embeddings_matrix
+
+def get_embeddings_layer(embeddings_matrix, name, max_len, trainable=False):
+    
+    embedding_layer = Embedding(
+        input_dim=embeddings_matrix.shape[0],
+        output_dim=embeddings_matrix.shape[1],
+        input_length=max_len,
+        weights=[embeddings_matrix],
+        trainable=trainable,
+        name=name
+    )
+        
+    return embedding_layer
