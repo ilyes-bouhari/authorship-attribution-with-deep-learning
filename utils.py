48baab8972f93ab3d8e3be1697cd970ffa52eb8a
ILYESBouhari <bouharidev.io@gmail.com>
2022-06-28 11:30:54 +0100

    update

--- utils.py
+++ utils.py
@@ -1,6 +1,28 @@
 import os
 import numpy as np
+import tensorflorw as tf
 from keras.layers import Embedding, Conv1D, MaxPooling1D, Flatten
+
+def normlize(text):
+
+    import re
+    import string
+    import contractions
+    nltk.download('stopwords')
+    from nltk.corpus import stopwords
+
+    result = tf.strings.lower(text)
+    result = tf.strings.regex_replace(result, "<[^>]+>", " ")
+    result = tf.strings.regex_replace(result, "\r\n", " ")
+    result = tf.strings.regex_replace(result, "https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+", "")
+
+    for index, (key, value) in enumerate(contractions.contractions_dict.items()):
+        result = tf.strings.regex_replace(result, r'\b(' + key + r')\b', value)
+
+    result = tf.strings.regex_replace(result, r'\b(' + r'|'.join(stopwords.words('english')) + r')\b\s*',"")
+    result = tf.strings.regex_replace(result, f"[{re.escape(string.punctuation)}]", "")
+
+    return result
 
 def load_glove_embeddings(path, embedding_dim):
 
@@ -71,3 +93,18 @@ def get_conv_pool(x_input, max_len, suffix, n_grams=[3, 4, 5], feature_maps=100)
         branches.append(branch)
 
     return branches
+
+def get_dataset_partitions_tf(ds, ds_size, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+    assert (train_split + test_split + val_split) == 1
+    
+    if shuffle:
+        ds = ds.shuffle(shuffle_size, seed=12)
+    
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+    
+    train_ds = ds.take(train_size)    
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
+    
+    return train_ds, val_ds, test_ds
